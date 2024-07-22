9b370fcd4d78d37bbbd9cace69102f69f00a8832
Ronmaccms <154700667+ronmaccms@users.noreply.github.com>
2024-07-22 18:44:58 -0400

    working version without sidebar

---
+++ my-project/src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<!-- src/components/AppHeader.vue -->
+<template>
+  <div class="header">
+    <h1>Project Title</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+};
+</script>
+
+<style scoped>
+.header {
+  background-color: black;
+  color: white;
+  padding: 10px;
+  text-align: center;
+}
+</style>
