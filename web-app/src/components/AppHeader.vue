df15914cc1aa1f2f62f66da26e215ce30a78b0b5
Ronmaccms <154700667+ronmaccms@users.noreply.github.com>
2024-08-30 12:19:49 -0400

    logo fix

--- web-app/src/components/AppHeader.vue
+++ web-app/src/components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header">
-    <img src="@/assets/logo.jpg" alt="Logo" class="logo" />
+    <img src="src\assets\img\logo-b.png" alt="Logo" class="logo" />
     <h1>WindFlowSim</h1>
   </div>
 </template>
